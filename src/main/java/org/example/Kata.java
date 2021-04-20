ecd9cc090efc5c05c82c4d1c83b55bfeb1941593
Josef Frola <xfrola@fi.muni.cz>
2021-04-20 11:48:51 +0200

    Implemented advantage reseting all score formats

--- src/main/java/org/example/Kata.java
+++ src/main/java/org/example/Kata.java
@@ -3,6 +3,7 @@ package org.example;
 class Kata {
     int aScore;
     int bScore;
+    String winner;
     boolean advantageA;
     boolean advantageB;
 
@@ -14,9 +15,21 @@ class Kata {
     }
 
     public void scoredA() {
-
-        if (aScore == 40){
+        if (advantageA) {
+            winner = "A";
+            return;
+        }
+        if (aScore == 40 && bScore == 40) {
+            if (advantageB) {
+                advantageB = false;
+                return;
+            }
             advantageA = true;
+            return;
+
+        }
+        if (aScore == 40){
+            winner = "A";
             return;
         }
         aScore += (aScore == 30) ? 10 : 15;
@@ -24,9 +37,20 @@ class Kata {
     }
 
     public void scoredB() {
-
-        if (bScore == 40){
+        if (advantageB) {
+            winner = "B";
+            return;
+        }
+        if (bScore == 40 && aScore == 40) {
+            if (advantageA) {
+                advantageA = false;
+                return;
+            }
             advantageB = true;
+            return;
+        }
+        if (bScore == 40){
+            winner = "B";
             return;
         }
         bScore += (bScore == 30) ? 10 : 15;
@@ -35,15 +59,21 @@ class Kata {
 
     public String showScore() {
 
+        if (winner != null) {
+            return String.format("winner: %s", winner);
+        }
+
         String stringA;
         String stringB;
 
         stringA = (advantageA) ? "A" : String.valueOf(aScore);
+        stringB = (advantageB) ? "A" : String.valueOf(bScore);
 
-        stringB = (advantageB) ? "B" : String.valueOf(bScore);
+        if (aScore != bScore || advantageA || advantageB) {
+            return String.format("%s - %s", stringA, stringB);
 
-        return String.format("%s - %s", stringA, stringB);
-
-
+        } else{
+            return (aScore == 40) ? "deuce" : String.format("%s all", stringA);
+        }
     }
 }
